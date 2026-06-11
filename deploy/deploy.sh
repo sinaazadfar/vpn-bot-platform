5432fff5aeeb5b58c32bffc409be091cc547b80c
sinaazadfar <sinaazadfar@gmail.com>
2026-06-11 16:12:36 +0330

    Initialize submodules during deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,7 @@ APP_DIR="${APP_DIR:-/opt/vpn-bot-platform}"
 cd "$APP_DIR"
 git fetch --all --prune
 git reset --hard "origin/${DEPLOY_BRANCH:-main}"
+git submodule update --init --recursive
 docker compose --profile migrate build migrate master-bot worker seller-bot
 docker compose --profile migrate up --force-recreate --abort-on-container-exit migrate
 docker compose up -d master-bot worker
